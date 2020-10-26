b55146f8829d472a05c3a3c950e534389c4edcba
Cedric Don Coloma <colomce@oocl.com>
2020-10-26 20:43:32 +0800

    [Counter][Cedric] feature: Added counter group with size props to display multiple counter

---
+++ src/components/CounterGroup.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Counter from './Counter';
+
+class CounterGroup extends Component {
+    
+    initArray(size) {
+        const number = size.length > 0 && parseInt(size) > 0 ? parseInt(size) : 0;
+        return Array.from(Array(number).keys());
+    }
+
+    render() {
+        const size = this.props.size;
+        const counterSizeArray = this.initArray(size);
+        const counters = counterSizeArray.map((value) => (
+            <Counter key={value} addToSum={this.props.addToSum}/>   
+        ));
+
+        return (
+            <div>
+                { counters }
+            </div>
+        );
+    }
+}
+
+export default CounterGroup;
