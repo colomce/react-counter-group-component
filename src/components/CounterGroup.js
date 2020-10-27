0800217c6fbef69f39abf340fa9729133a0bbd1e
Cedric Don Coloma <colomce@oocl.com>
2020-10-27 15:15:20 +0800

    [Counter][Cedric-JC] feature: removed the addToSum method in Counter props

--- src/components/CounterGroup.js
+++ src/components/CounterGroup.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Counter from './Counter';
 
 class CounterGroup extends Component {
-    
+
     initArray(size) {
         const number = size.length > 0 && parseInt(size) > 0 ? parseInt(size) : 0;
         return Array.from(Array(number).keys());
@@ -12,7 +12,7 @@ class CounterGroup extends Component {
         const size = this.props.size;
         const counterSizeArray = this.initArray(size);
         const counters = counterSizeArray.map((value) => (
-            <Counter key={value} addToSum={this.props.addToSum}/>   
+            <Counter key={value}/>   
         ));
 
         return (
