b87cfa4804835d27500155fd29f86f6177118231
Cedric Don Coloma <colomce@oocl.com>
2020-10-27 15:24:06 +0800

    [Counter][Cedric-JC] feature: call decrease sum counter component

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -16,7 +16,7 @@ class Counter extends Component {
     }
 
     onDecrease = () => {
-        this.setState((prevState) => ({number : prevState.number - 1}), () => this.props.addToSum(-1));
+        this.setState((prevState) => ({number : prevState.number - 1}), () => this.props.decreaseSum());
     }
 
     render() {
