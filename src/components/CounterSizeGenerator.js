68259248884e5c6a9468c5a1da605646b63e103a
Cedric Don Coloma <colomce@oocl.com>
2020-10-27 15:10:20 +0800

    [Counter][Cedric-JC] feature: Call the updateCounterSize in CounterSizeGenerator

--- src/components/CounterSizeGenerator.js
+++ src/components/CounterSizeGenerator.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 
 class CounterSizeGenerator extends Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {size: 0}
-    }
     
     onChange = (event) => {
         const value = event.target.value;
         this.setState(() => {
             return {size: value}
         }, () => {
-            this.props.onSizeChange(value);
+            this.props.updateCounterSize(value);
         });
     }
 
@@ -23,7 +18,6 @@ class CounterSizeGenerator extends Component {
                 <input  type="number"
                         name="size"
                         id="size"
-                        value= {this.state.size}
                         onChange={this.onChange} />
                 <label htmlFor="size"> Counters </label>
             </div>
