df95f1cec20829b5ba36b11bdb3f1c8ec0736314
Cedric Don Coloma <colomce@oocl.com>
2020-10-27 15:16:43 +0800

    [Counter][Cedric-JC] feature: added increaseSum action creator

--- src/action.js
+++ src/action.js
@@ -1,3 +1,7 @@
 export const updateCounterSize = (size) => {
     return {type: "UPDATE_COUNTER_SIZE", payload: size};
 }
+
+export const increaseSum = () => {
+    return {type: "INCREASE_SUM"};
+}
