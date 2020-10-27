c0571ae9cd892b7024b74e679952e55070e04ae6
Cedric Don Coloma <colomce@oocl.com>
2020-10-27 15:13:17 +0800

    [Counter][Cedric-JC] feature: Created a container for CounterGroup

---
+++ src/containers/CounterGroupContainer.js
@@ -0,0 +1,9 @@
+import { connect } from 'react-redux';
+import CounterGroup from '../components/CounterGroup';
+
+const mapStateToProps = (state) => ({
+    size: state.size
+});
+
+const CounterGroupContainer = connect(mapStateToProps)(CounterGroup);
+export default CounterGroupContainer;
