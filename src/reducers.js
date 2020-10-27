7a521c1c07f4734d067fef7b01a5279568cbbd7c
Cedric Don Coloma <colomce@oocl.com>
2020-10-27 15:23:30 +0800

    [Counter][Cedric-JC] feature: Added decrease sum reducer

--- src/reducers.js
+++ src/reducers.js
@@ -14,7 +14,8 @@ const sum = (state = 0, action) => {
     switch(action.type) {
         case "INCREASE_SUM" :
             return ++state;
-
+        case "DECREASE_SUM" :
+            return --state;
         default:
             return state;
     }
