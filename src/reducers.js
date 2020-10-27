0c7f7deb92712fb341f5b217d52c5202ee4060d1
Cedric Don Coloma <colomce@oocl.com>
2020-10-27 15:00:13 +0800

    [Counter][Cedric-JC] feature: Added reducer for size UPDATE_COUNTER_SIZE

---
+++ src/reducers.js
@@ -0,0 +1,15 @@
+import { combineReducer } from 'redux';
+
+const size = (state = 0, action) => {
+    switch(action.type) {
+        case "UPDATE_COUNTER_SIZE" :
+            return action.payload;
+
+        default:
+            return state;
+    }
+}
+
+export default combineReducer({
+    size
+});
