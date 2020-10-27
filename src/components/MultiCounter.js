03a76fc1c1dedf36c3edf45f0dc9c0b009463278
Cedric Don Coloma <colomce@oocl.com>
2020-10-27 15:15:57 +0800

    [Counter][Cedric-JC] feature: removed the constructor, addToSum and used CounterGroupContainer

--- src/components/MultiCounter.js
+++ src/components/MultiCounter.js
@@ -1,29 +1,17 @@
 import React, { Component } from 'react';
 import CounterGroupSum from './CounterGroupSum';
-import CounterGroup from './CounterGroup';
 import CounterSizeGeneratorContainer from '../containers/CounterSizeGeneratorContainer';
+import CounterGroupContainer from '../containers/CounterGroupContainer';
 
 class MultiCounter extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            sum : 0, 
-            size: 0
-        }
-    }
-    
-    addToSum = (valueToAdd) => {
-        this.setState((prevState) => ({sum : prevState.sum + valueToAdd}));
-    }
-
     render() {
         return (
             <div>
                 <fieldset>
                     <CounterSizeGeneratorContainer />
-                    <CounterGroupSum sum={this.state.sum}/>
+                    <CounterGroupSum/>
                 </fieldset>
-                <CounterGroup size={this.state.size} addToSum={this.addToSum}/>
+                <CounterGroupContainer/>
             </div>
         );
     }
