4c125c34c452fb553682109eeba698af7a16fcff
Cedric Don Coloma <colomce@oocl.com>
2020-10-27 15:25:55 +0800

    [Counter][Cedric-JC] feature: Added CounterGroupSumContainer

---
+++ src/containers/CounterGroupSumContainer.js
@@ -0,0 +1,9 @@
+import { connect } from 'react-redux';
+import CounterGroupSum from '../components/CounterGroupSum';
+
+const mapStateToProps = (state) => ({
+    sum: state.sum
+});
+
+const CounterGroupSumContainer = connect(mapStateToProps)(CounterGroupSum);
+export default CounterGroupSumContainer;
